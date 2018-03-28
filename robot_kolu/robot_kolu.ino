0f34a25e88e4f5bbcedfb417b1cee1e2db12462f
LAB12-09\Lab12-09 <yilmaz.alagoz@boun.edu.tr>
2018-03-28 11:21:12 +0300

    refactoring

--- robot_kolu/robot_kolu.ino
+++ robot_kolu/robot_kolu.ino
@@ -1,53 +1,76 @@
 #include <Servo.h>
-Servo govdemotor,kolmotor,dirsekmotor,tirnakmotor;
+
 byte hiz=10;
+Servo servoParts[] = {bodyMotor,armMotor,elbowMotor,gripperMotor};
+void attachParts() {
+  bodyMotor.attach(3, 1000, 2000);
+  armMotor.attach(5, 1000, 2000);
+  elbowMotor.attach(6, 1000, 2000);
+  gripperMotor.attach(9, 1000, 2000);  
+}
+
+void 
+
 void setup() {
-govdemotor.attach(3, 1000, 2000);
-kolmotor.attach(5, 1000, 2000);
-dirsekmotor.attach(6, 1000, 2000);
-tirnakmotor.attach(9, 1000, 2000);
-govdemotor.write(90);
-kolmotor.write(90);
-dirsekmotor.write(90);
-tirnakmotor.write(90);
-}
 
+  attachParts()
+  
+  bodyMotor.write(90);
+  armMotor.write(90);
+  elbowMotor.write(90);
+  gripperMotor.write(90);
+}
 void loop() {
-  calibre(90,175);
- for(byte derece=90;derece<=175;derece++)
-  {
-govdemotor.write(derece);
-delay(hiz);
+ calibre(90,175);
+  for(byte derece=90;derece<=175;derece++){
+    bodyMotor.write(derece);
+    delay(hiz);
   }
-    for(byte derece=170;derece>=5;derece--)
-  {
-kolmotor.write(derece);
-delay(hiz);
+  for(byte derece=170;derece>=5;derece--){
+    armMotor.write(derece);
+    delay(hiz);
   }
-    for(byte derece=5;derece<=170;derece++)
-  {
-kolmotor.write(derece);
-delay(hiz);
+  for(byte derece=90;derece>=35;derece--){
+    elbowMotor.write(derece);
+    delay(hiz);
   }
-   for(byte derece=175;derece>=90;derece--)
-  {
-govdemotor.write(derece);
-delay(hiz);
+  for(byte derece=90;derece<=175;derece++){
+    tirnakmotor.write(derece);
+    delay(hiz);
   }
- for(byte derece=90;derece>=70;derece--)
-  {
-dirsekmotor.write(derece);
-delay(50);
+  for(byte derece=5;derece<=170;derece++){
+    armMotor.write(derece);
+    delay(hiz);
   }
-   for(byte derece=90;derece<=120;derece++)
-  {
-tirnakmotor.write(derece);
-delay(50);
+  for(byte derece=175;derece>=5;derece--){
+    bodyMotor.write(derece);
+    delay(hiz);
+  }
+  //masaya birakmak için
+  for(byte derece=170;derece>=5;derece--){
+    armMotor.write(derece);
+    delay(hiz);
+  }
+  for(byte derece=35;derece<=90;derece++){
+    elbowMotor.write(derece);
+    delay(hiz);
+  }
+  for(byte derece=175;derece>=90;derece--){
+    tirnakmotor.write(derece);
+    delay(hiz);
+  }
+  for(byte derece=5;derece<=170;derece++){
+    armMotor.write(derece);
+    delay(hiz);
+  }
+  
+  for(byte derece=5;derece<=90;derece++){
+    bodyMotor.write(derece);
+    delay(hiz);
   }
 }
 
-byte calibre(byte basla, byte bitis)
-{
+byte calibre(byte basla, byte bitis){
   
 }
 
